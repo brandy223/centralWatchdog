ab084b5e683909fda385aa24a904db6952fa7043
brandy <brandan.le-gall@isen.yncrea.fr>
2023-07-11 12:46:24 +0200

    feat: new version of index : serverDatabase init and webServerInit + listening of server port and broadcast emit

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,58 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const http = require('http').createServer(app);
-
 const Network = require('./utils/Network');
 const Database = require('./utils/Database');
 
+const express = require('express');
+const app = express();
+const bodyParser = express.json();
+const http = require('http')
+
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    optionsSuccessStatus: 200,
+    credentials: true
+}
+
+app.use(bodyParser);
+// app.use(bodyParser.urlencoded({extended: true}));
+
 async function main(): Promise<void> {
+    await serverDatabaseInit();
+    const io = await webServerInit();
+
+    // app.post('/', async (req: any, res: any) => {
+    //     console.log(req.body);
+    //     res.body = { status: "OK" };
+    //     res.send(res.body);
+    // });
+
+    io.sockets.on('connection', (socket: any) => {
+        socket.on("open_session", function (session: any) {
+            console.log(session);
+            socket.session = session;
+            console.log("socket.session à la connexion: ");
+            console.log(socket.session);
+        });
+
+        socket.on("disconnect", function () {
+            console.log("socket.session à la déconnexion: ");
+            console.log(socket.session);
+        });
+
+        socket.on("message", function (message: object) {
+            console.log(message);
+            socket.broadcast.emit("message", message);
+        });
+    });
+}
+
+main();
+
+async function serverDatabaseInit(): Promise<void> {
+    let serverPriority = 1;
 
     // GET LOCAL IP
     const ip = await Network.getLocalIP();
@@ -19,21 +62,42 @@ async function main(): Promise<void> {
         console.log(`Local IP: ${ip}`);
 
     // VERIFY SERVER EXISTS IN DATABASE
-    const isServerInDatabase = await Database.isServerInDatabase(ip);
-    if (!isServerInDatabase) {
-        await Database.addServerToDatabase(ip, "Central", Number(process.env.SERVER_PORT));
+    if (await Database.isThereAnotherCentralServer(ip)) serverPriority = 0;
+
+    if (!await Database.isServerInDatabase(ip)) {
+        await Database.addServerToDatabase(ip, "Central", Number(process.env.SERVER_PORT), serverPriority);
         console.log(`Added node server to database`);
     } else {
-        const isServerACentral = await Database.isServerCentral(ip);
-        if (!isServerACentral) {
+        if (!await Database.isServerCentral(ip)) {
             await Database.updateServerType(ip, "Central");
+            console.log(`Updated server type to Central`);
         }
-        const isPortSet = await Database.isPortSet(ip);
-        if (!isPortSet) {
-            // await Database.update(ip, process.env.SERVER_PORT);
-            await Database.updateServerPort(ip, process.env.SERVER_PORT);
+        if (!await Database.isPortSet(ip)) {
+            await Database.updateServerPort(ip, Number(process.env.SERVER_PORT));
+            console.log(`Updated server port to ${process.env.SERVER_PORT}`);
+        }
+        else {
+            const port = await Database.getServerByIP(ip);
+            if (port !== Number(process.env.SERVER_PORT)) {
+                await Database.updateServerPort(ip, Number(process.env.SERVER_PORT));
+                console.log(`Updated server port to ${process.env.SERVER_PORT}`);
+            }
+        }
+        if (!await Database.isServerPrioritySet(ip)) {
+            await Database.updateServerPriority(ip, serverPriority);
+            console.log(`Updated server priority to ${serverPriority}`);
         }
     }
 }
 
-main();
+async function webServerInit(): Promise<any> {
+    // WEB SERVER SETUP
+    const server = http.createServer(app);
+    server.listen(process.env.SERVER_PORT, () => {
+        console.log(`Server listening on port ${process.env.SERVER_PORT}`);
+    });
+    return require('socket.io')(server, {
+        cors: corsOptions,
+        allowEIO3: true
+    });
+}
