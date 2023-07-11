f4c325a5c91a389a75c48c870efdbf443374ebed
brandy <brandan.le-gall@isen.yncrea.fr>
2023-07-11 17:48:03 +0200

    fix: adapted socket io server declaration to package version refactor: changed statements arrangement

--- src/index.ts
+++ src/index.ts
@@ -7,22 +7,34 @@ const Database = require('./utils/Database');
 
 const express = require('express');
 const app = express();
-const bodyParser = express.json();
 const http = require('http')
+const { Server } = require("socket.io");
 
 const corsOptions = {
+    // origin: '192.168.10.58',
     origin: '*',
     methods: ['GET', 'POST'],
     optionsSuccessStatus: 200,
     credentials: true
 }
 
-app.use(bodyParser);
+// app.use(express.json());
 // app.use(bodyParser.urlencoded({extended: true}));
 
 async function main(): Promise<void> {
     await centralServerDatabaseInit();
-    const io = await centralWebServerInit();
+
+    const server = http.createServer(app);
+    const io = new Server(server, {
+        cors: corsOptions,
+        allowEIO3: true, // false by default
+    });
+
+    console.log(io);
+
+    io.on("error", function () {
+        console.log("error");
+    });
 
     // app.post('/', async (req: any, res: any) => {
     //     console.log(req.body);
@@ -30,23 +42,28 @@ async function main(): Promise<void> {
     //     res.send(res.body);
     // });
 
-    io.sockets.on('connection', (socket: any) => {
-        socket.on("open_session", function (session: any) {
-            console.log(session);
-            socket.session = session;
-            console.log("socket.session à la connexion: ");
-            console.log(socket.session);
-        });
+    io.on('connection', (socket: any) => {
+        console.log("New connection");
+        // socket.on("open_session", function (session: any) {
+        //     console.log(session);
+        //     socket.session = session;
+        //     console.log("socket.session à la connexion: ");
+        //     console.log(socket.session);
+        // });
+        //
+        // socket.on("disconnect", function () {
+        //     console.log("socket.session à la déconnexion: ");
+        //     console.log(socket.session);
+        // });
+        //
+        // socket.on("message", function (message: object) {
+        //     console.log(message);
+        //     socket.broadcast.emit("message", message);
+        // });
+    });
 
-        socket.on("disconnect", function () {
-            console.log("socket.session à la déconnexion: ");
-            console.log(socket.session);
-        });
-
-        socket.on("message", function (message: object) {
-            console.log(message);
-            socket.broadcast.emit("message", message);
-        });
+    server.listen(process.env.SERVER_PORT, () => {
+        console.log(`Server listening on port ${process.env.SERVER_PORT}`);
     });
 }
 
@@ -61,35 +78,38 @@ async function centralServerDatabaseInit(): Promise<void> {
     else
         console.log(`Local IP: ${ip}`);
 
-    // VERIFY SERVER EXISTS IN DATABASE
-    if (await Database.isThereAnotherCentralServer(ip)) serverPriority = 0;
+    // VERIFY IF ANOTHER CENTRAL SERVER EXISTS IN DATABASE
+    if (await Database.isThereAnotherCentralServer(ip)) {
+        serverPriority = 0;
+    }
 
+    // VERIFY SERVER EXISTS IN DATABASE
     if (!await Database.isServerInDatabase(ip)) {
         await Database.addServerToDatabase(ip, "Central", Number(process.env.SERVER_PORT), serverPriority);
         console.log(`Added node server to database`);
-    } else {
-        if (!await Database.isServerCentral(ip) || !await Database.isPortSet(ip) || !await Database.isServerPrioritySet(ip)) {
+        return;
+    }
+
+    // IF SERVER ALREADY IN DATABASE
+    if (!await Database.isServerCentral(ip) || !await Database.isPortSet(ip) || !await Database.isServerPrioritySet(ip)) {
+        await Database.updateServer(ip, "Central", Number(process.env.SERVER_PORT), serverPriority);
+        console.log(`Updated server information`);
+        return;
+    }
+
+    // IF SERVERS INFORMATION ARE CORRECT
+    if (await Database.isPortSet(ip)) {
+        const server = await Database.getServerByIP(ip);
+        if (server.port !== Number(process.env.SERVER_PORT)) {
             await Database.updateServer(ip, "Central", Number(process.env.SERVER_PORT), serverPriority);
-            console.log(`Updated server information`);
-        }
-        else if (await Database.isPortSet(ip)) {
-            const server = await Database.getServerByIP(ip);
-            if (server.port !== Number(process.env.SERVER_PORT)) {
-                await Database.updateServer(ip, "Central", Number(process.env.SERVER_PORT), serverPriority);
-                console.log(`Updated server port to ${process.env.SERVER_PORT}`);
-            }
+            console.log(`Updated server port to ${process.env.SERVER_PORT}`);
         }
     }
-}
-
-async function centralWebServerInit(): Promise<any> {
-    // WEB SERVER SETUP
-    const server = http.createServer(app);
-    server.listen(process.env.SERVER_PORT, () => {
-        console.log(`Server listening on port ${process.env.SERVER_PORT}`);
-    });
-    return require('socket.io')(server, {
-        cors: corsOptions,
-        allowEIO3: true
-    });
+    if (await Database.isServerPrioritySet(ip)) {
+        const server = await Database.getServerByIP(ip);
+        if (server.priority !== serverPriority) {
+            await Database.updateServer(ip, "Central", Number(process.env.SERVER_PORT), serverPriority);
+            console.log(`Updated server priority to ${serverPriority}`);
+        }
+    }
 }
