1c73e1b2ea84c1485aabc69dd85c6a291e66e7c3
brandy <brandan.le-gall@isen.yncrea.fr>
2023-07-18 16:07:13 +0200

    feat: get the highest value in a map

---
+++ src/utils/Map.ts
@@ -0,0 +1,19 @@
+
+/**
+ * Function to get the highest priority state value in a map (key, [boolean, priority])
+ * @param {Map<number, [boolean, number]>} map The map to get the highest priority state value from
+ * @returns {number} The key to the highest priority state value
+ * @throws {Error} If the map is empty
+ */
+export function getHighestPriorityStateValue(map: Map<number, [boolean, number]>): number {
+    if (map.size === 0) throw new Error("Map is empty");
+    let highestPriority: number = 0;
+    let highestPriorityKey = -1;
+    for (const [key, [boolean, priority]] of map.entries()) {
+        if (boolean && priority > highestPriority) {
+            highestPriority = priority;
+            highestPriorityKey = key;
+        }
+    }
+    return highestPriorityKey;
+}
