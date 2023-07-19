eef17aed809bd065d0e7ae67862dc4d4c4e16928
brandy <brandan.le-gall@isen.yncrea.fr>
2023-07-19 16:58:35 +0200

    feat: advanced state of scenario handler algo

--- src/utils/Message.ts
+++ src/utils/Message.ts
@@ -1,11 +1,12 @@
 
 const Database = require('./Database');
-const MapUtils = require('./Utilities/Map');
+const MapUtils = require('./utilities/Map');
 const theme = require('./ColorScheme').theme;
 
-const sendMessage = require('../Actions/SendMessage').sendMessage;
-const sendEmail = require('../Actions/SendEmail').sendEmail;
-const sendGlobalMessage = require('../Actions/SendGlobalMessage').sendGlobalMessage;
+const sendMessage = require('../actions/SendMessage').sendMessage;
+const sendEmail = require('../actions/SendEmail').sendEmail;
+const sendGlobalMessage = require('../actions/SendGlobalMessage').sendGlobalMessage;
+const reboot = require('../actions/Reboot').reboot;
 
 /**
  * Parse message from server and execute the corresponding action
@@ -40,15 +41,20 @@ async function serviceMessageHandler(message: any): Promise<void> {
 
     const stateValuesMap = new Map<number, [boolean, number]>();
     for (const stateValue of stateValues) {
+        let isStateValueTrue = false;
         switch(stateValue.typeOfValue) {
             case 1:
-                let isStateValueTrue = false;
                 if (message.status[1].includes(stateValue.value)) isStateValueTrue = true;
                 stateValuesMap.set(stateValue.id, [isStateValueTrue, Number(stateValue.priority)]);
                 break;
             case 2:
+                // TODO : Check if it's the right way to do it - 0 or 1 ???
+                if (message.status[0] < stateValue.value) isStateValueTrue = true;
+                stateValuesMap.set(stateValue.id, [isStateValueTrue, Number(stateValue.priority)]);
                 break;
             case 3:
+                if (message.status[0] > stateValue.value) isStateValueTrue = true;
+                stateValuesMap.set(stateValue.id, [isStateValueTrue, Number(stateValue.priority)]);
                 break;
             default:
                 console.log(theme.error("Unknown type of value"));
@@ -62,6 +68,26 @@ async function serviceMessageHandler(message: any): Promise<void> {
     const scenario = await Database.getScenarioById(stateValue.scenarioId);
     const actionsIds: any[] = await Database.getScenarioActionsIds(scenario.id);
     const actions: any[] = await Database.getActionsByIds(actionsIds);
-    // Execute actions
-
+    for (const action of actions) {
+        switch (action.name.toLowerCase()) {
+            case "sendmail":
+                // TODO: DETERMINE RECEIVER BEFORE SENDING
+                await sendMessage(action, message);
+                break;
+            case "sendmessage":
+                // TODO: DETERMINE RECEIVER BEFORE SENDING
+                await sendEmail(action, message);
+                break;
+            case "sendglobalmessage":
+                await sendGlobalMessage(action, message);
+                break;
+            case "reboot":
+                const server = await Database.getServerById(message.server.id);
+                await reboot(server.ipAddr, process.env.SSH_USER);
+                break;
+            default:
+                console.log(theme.error("Unknown type of action"));
+                break;
+        }
+    }
 }
