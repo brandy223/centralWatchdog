e013d3f9a6ad0a3fc25ba6df5b3353561c968c72
brandy <brandan.le-gall@isen.yncrea.fr>
2023-07-20 16:40:46 +0200

    feat: can now know if a person is free or not

--- src/actions/Utilities.ts
+++ src/actions/Utilities.ts
@@ -1,11 +1,20 @@
 
+const axios = require('axios');
+
 /**
- * Verify if someone is free to receive a message
+ * Verify if someone is free to receive a message / mail
  * @param {number} id The id of the person
  * @returns {Promise<boolean>} True if the person is free, false otherwise
- * @throws {Error} If the id is null or undefined
- * @throws {Error} If the person is not in the database
+ * @throws {Error} If the person ID does not exist
  */
 export async function isPersonFree (id: number) : Promise<boolean> {
-    return false;
+    const today = new Date();
+    const day = today.getDate();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    let apiUrl = `http://manager.reseau.lan/index.php/checkDispoUserByDate?date-debut=${month}/${day}/${year}&id=${id}`
+    const res = await axios.get(apiUrl);
+
+    if (JSON.stringify(res.data).includes("undefined")) throw new Error("Person ID does not exist");
+    return !JSON.stringify(res.data).includes("Absent");
 }
