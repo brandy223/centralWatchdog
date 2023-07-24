036f8daa96fea9720ef1f81c48cf5965b4ea1eee
brandy <brandan.le-gall@isen.yncrea.fr>
2023-07-24 16:01:24 +0200

    feat: send sms function

--- src/actions/SendMessage.ts
+++ src/actions/SendMessage.ts
@@ -1,14 +1,12 @@
 
+const axios = require('axios').default;
+
 /**
  * Send a message to someone
  * @param {string} number The number to send the message to
  * @param {string} message The message to send
  * @returns {Promise<void>}
- * @throws {Error} If the number is null or undefined
- * @throws {Error} If the message is null or undefined
- * @throws {Error} If the number is not valid
- * @throws {Error} If the number is not reachable
  */
 export async function sendMessage (number: string, message: string) : Promise<void> {
-
+    await axios.post(process.env.SEND_SMS_ROUTE + "?numeros=[\"" + number + "\"]&message=" + message);
 }
