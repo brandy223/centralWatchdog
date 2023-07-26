7efde30d1f94bb3f685ba7bd4afee9aaf5d004ae
brandy <brandan.le-gall@isen.yncrea.fr>
2023-07-26 11:45:57 +0200

    refactor: function modified and moved to Actors in database utils folder

--- src/utils/utilities/Array.ts
+++ src/utils/utilities/Array.ts
@@ -32,27 +32,3 @@ export async function compareArrays(a: any[], b: any[]): Promise<boolean> {
     }
     return true;
 }
-
-/**
- * Returns the indexes of the highest priority actors
- * @param {ActorsForScenarios[]} actors The actors to get the highest priority from
- * @return {Promise<ActorsForScenarios[]>} The actors with the highest priority
- * @throws {Error} If no actors are given
- */
-export async function getHighestPriorityActors(actors: ActorsForScenarios[]): Promise<ActorsForScenarios[]> {
-    if (actors.length === 0) throw new Error("No actors given");
-    const highestPriority: number = Math.min(...actors.map((actor: ActorsForScenarios): number => actor.priority));
-    return actors.filter((actor: ActorsForScenarios): boolean => actor.priority === highestPriority);
-}
-
-/**
- * Returns the indexes of the highest priority actors lists
- * @param {ActorsListsForScenarios[]} actorsLists The actors lists to get the highest priority from
- * @return {Promise<ActorsListsForScenarios[]>} The actors lists with the highest priority
- * @throws {Error} If no actors lists are given
- */
-export async function getHighestPriorityActorsLists(actorsLists: ActorsListsForScenarios[]): Promise<ActorsListsForScenarios[]> {
-    if (actorsLists.length === 0) throw new Error("No actors lists given");
-    const highestPriority: number = Math.min(...actorsLists.map((actorList: ActorsListsForScenarios): number => actorList.priority));
-    return actorsLists.filter((actorList: ActorsListsForScenarios): boolean => actorList.priority === highestPriority);
-}
