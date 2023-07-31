557868f143d755d103a1635cb8c1e05955226daa
brandy <brandan.le-gall@isen.yncrea.fr>
2023-07-31 11:22:06 +0200

    feat: execute list of timed tasks and clear them if needed

---
+++ src/utils/Timer.ts
@@ -0,0 +1,39 @@
+
+/**
+ * Execute a timed task
+ * @param {any[]} functions
+ * @param {number[]} intervals
+ * @return {Promise<any[]>} Array of setIntervals variables
+ * @throws {Error} If the number of functions, intervals and timeouts is not the same
+ */
+export async function executeTimedTask(functions: (() => void)[], intervals: number[]): Promise<any[]> {
+    if (functions.length !== intervals.length)
+        throw new Error("The number of functions, intervals and timeouts must be the same");
+
+    const intervalsTabs: any[] = [];
+
+    for (let i = 0; i < functions.length; i++) {
+        const fct = functions[i];
+        const interval: number = intervals[i];
+
+        const intervalVar = setInterval((): void => {
+            fct();
+        }, interval);
+
+        intervalsTabs.push(intervalVar);
+    }
+    return intervalsTabs;
+}
+
+/**
+ * Clear all intervals in an array
+ * @param {any[]} intervalsTab Array of setIntervals functions
+ * @return {void}
+ * @throws {Error} If the array is empty
+ */
+export function clearAllIntervals(intervalsTab: any[]): void {
+    if (intervalsTab.length === 0) throw new Error("The intervals array is empty");
+    for (const interval of intervalsTab) {
+        clearInterval(interval);
+    }
+}
